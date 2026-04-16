345077c10e7e8bc07f7253eae1bc2c4df05c5a96
GISELLE ORTIZ CALVILLO <gortizc4@miumg.edu.gt>
2026-04-15 21:51:50 -0600

    Iniciando Ejercicio 5

---
+++ src/HerenciaAplicada/Empleado.java
@@ -0,0 +1,30 @@
+package HerenciaAplicada;
+
+public class Empleado {
+	protected String nombre;
+	protected double salario;
+	//CONSTRUCTOR
+	public Empleado(String nombre, double salario) {
+		super();
+		this.nombre = nombre;
+		this.salario = salario;
+	}
+	//METODO
+	public double calcularsalario() {
+		return salario; 
+	}
+	
+		
+	
+	
+	public void mostrarDatos() {
+		System.out.println("Nombre"+ nombre);
+		System.out.println("Salario"+ calcularsalario());
+		
+	}
+	
+	
+	
+	
+
+}
---
+++ src/HerenciaAplicada/Gerente.java
@@ -0,0 +1,17 @@
+package HerenciaAplicada;
+
+public class Gerente extends Empleado {
+	
+	private double bonoadicional;
+
+	public Gerente(String nombre, double salario, double bonoadicional) {
+		super(nombre, salario);
+		this.bonoadicional = bonoadicional;
+	}
+
+	
+	
+	
+
+}
+ 
