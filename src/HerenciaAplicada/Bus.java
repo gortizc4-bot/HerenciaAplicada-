082dff5bec3197429f63236687a751dcbcadf988
GISELLE ORTIZ CALVILLO <gortizc4@miumg.edu.gt>
2026-04-15 19:11:54 -0600

    Class Bus terminada

--- src/HerenciaAplicada/Bus.java
+++ src/HerenciaAplicada/Bus.java
@@ -1,7 +1,22 @@
 package HerenciaAplicada;
 
 public class Bus extends Trasporte{
-	private String ruta; 
+	
+	private String ruta;
+	
+//CONSTRUCTOR
+	public Bus(int capacidad, String ruta) {
+		super(capacidad);
+		this.ruta = ruta;
+	}
+	@Override
+	public void descripcion() {
+		super.descripcion();
+		System.out.println("Ruta"+ ruta);
+	} 
+	
+	
+	
 	
 
 }
