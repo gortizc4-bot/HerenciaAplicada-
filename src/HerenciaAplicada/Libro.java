9664566ca87febf2ccc47ba3eb101e233ad998b6
GISELLE ORTIZ CALVILLO <gortizc4@miumg.edu.gt>
2026-04-15 23:15:33 -0600

    Iniciando Ejercicio 9 Libro y LibroDigital

---
+++ src/HerenciaAplicada/Libro.java
@@ -0,0 +1,13 @@
+package HerenciaAplicada;
+
+public class Libro {
+protected String titulo; 
+protected String autor;
+
+public Libro(String titulo, String autor) {
+	super();
+	this.titulo = titulo;
+	this.autor = autor;
+} 
+
+}
